647be4ff42a209bc1b1670a9631332a4f054c9f2
rfans666 <105523622+rfans666@users.noreply.github.com>
2022-10-24 20:24:50 -0400

    Simple Button + Background

---
+++ Lesson2/Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Jiayu Han on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
