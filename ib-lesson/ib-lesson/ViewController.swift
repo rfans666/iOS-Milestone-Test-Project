b2796f65b0bde6316c3ed81576639a1e31e2d25f
rfans666 <105523622+rfans666@users.noreply.github.com>
2022-10-31 19:43:20 -0400

    test

--- ib-lesson/ib-lesson/ViewController.swift
+++ ib-lesson/ib-lesson/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var lbl: UILabel!
     
     
@@ -32,30 +32,24 @@ class ViewController: UIViewController {
         else{
             print("Error: Out of Bound Random Variable")
         }
-    
+        
         let randomNum2 = Int.random(in: 1...3)
         if randomNum2 == 1 {
-            btn.setTitle("Click Here", for: .normal)
+            sender.setTitle("Click Here", for: .normal)
         }
         else if randomNum2 == 2 {
-            btn.setTitle("Who's Next", for: .normal)
+            sender.setTitle("Who's Next", for: .normal)
         }
         else if randomNum2 == 3 {
-            btn.setTitle("Please Click", for: .normal)
+            sender.setTitle("Please Click", for: .normal)
         }
         else {
             print("Error: Button Var Out of Range")
         }
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    override func viewDidDisappear(_ animated: Bool) {
-        <#code#>
-    }
-
 }
 
